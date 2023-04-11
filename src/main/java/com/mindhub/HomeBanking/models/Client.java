037e89b1a89d547890304f9a47de4ce0f25e1dff
Luisa <luisafmv321@gmail.com>
2023-04-10 23:02:26 -0500

    Client, Accounts and transactions 3

--- src/main/java/com/mindhub/HomeBanking/models/Client.java
+++ src/main/java/com/mindhub/HomeBanking/models/Client.java
@@ -1,6 +1,5 @@
 package com.mindhub.HomeBanking.models;
 
-import com.mindhub.HomeBanking.dtos.AccountDTO;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
@@ -17,7 +16,7 @@ public class Client {
     private String lastName;
     private String email;
     @OneToMany(mappedBy = "client", fetch = FetchType.EAGER)
-    Set<Account> accounts = new HashSet<>();
+    private Set<Account> accounts = new HashSet<>();
     public Client(){}
 
     public Client(String first, String last){
