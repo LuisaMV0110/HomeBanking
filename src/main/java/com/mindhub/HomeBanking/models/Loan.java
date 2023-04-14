952465d28eb46c26ea936e89fe5541175d1531a0
Luisa <luisafmv321@gmail.com>
2023-04-14 00:07:01 -0500

    Loans 1

---
+++ src/main/java/com/mindhub/HomeBanking/models/Loan.java
@@ -0,0 +1,70 @@
+package com.mindhub.HomeBanking.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Entity
+public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+    private String name;
+    private double maxAmount;
+    @ElementCollection
+    private Set<Integer> payments = new HashSet<>();
+    @OneToMany(mappedBy = "loan", fetch = FetchType.EAGER)
+    private Set<ClientLoan> clientLoans = new HashSet<>();
+    public Loan(){}
+    public Loan(String name, double maxAmount, Set<Integer> payments) {
+        this.name = name;
+        this.maxAmount = maxAmount;
+        this.payments = payments;
+    }
+    @JsonIgnore
+    public Set<Client> getClients(){
+        return clientLoans.stream().map(clientLoan -> clientLoan.getClient()).collect(Collectors.toSet());
+    }
+    public Set<ClientLoan> getClientLoans() {
+        return clientLoans;
+    }
+    public void setClientLoans(Set<ClientLoan> clientLoans) {
+        this.clientLoans = clientLoans;
+    }
+    public long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getMaxAmount() {
+        return maxAmount;
+    }
+    public void setMaxAmount(double maxAmount) {
+        this.maxAmount = maxAmount;
+    }
+    public Set<Integer> getPayments() {
+        return payments;
+    }
+    public void addClientLoan(ClientLoan clientLoan) {
+        clientLoan.setLoan(this);
+        clientLoans.add(clientLoan);
+    }
+    @Override
+    public String toString() {
+        return "Loan{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", maxAmount=" + maxAmount +
+                ", payments=" + payments +
+                '}';
+    }
+}
